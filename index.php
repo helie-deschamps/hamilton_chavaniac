968b329d76e58f91226e0c13fdf8fb31aa6e7d47
rafaelynk <rafael.yenokyan1@gmail.com>
2023-12-14 15:56:16 +0100

    initialisation du projet

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE php>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   
+    
+    <h1>Hamilton, la comédie musicale</h1>
+
+    <figure>
+        <img src=" " alt=""> 
+        <figcaption>Une première en France</figcaption>
+    </figure>
+
+    <p>biuezgkqhjvdhizhd</p>
+ 
+<!-- mettre le caroussel de chansons-->
+
+    <p>biuezgkqhjvdhizhd</p>
+
+    <iframe id="vidéotrailer" width="" height="" src="">  </iframe>
+     
+<footer>
+
+</footer>
+
+</body>
+</html>
