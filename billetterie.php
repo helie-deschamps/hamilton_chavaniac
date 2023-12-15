b0a1203429a54c86fd5a8e03fc76a6accec9f3c0
loucortial <lou.cortial@gmail.com>
2023-12-15 10:35:11 +0100

    création billetterie

---
+++ billetterie.php
@@ -0,0 +1,36 @@
+<?php
+include "header.html";
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Billetterie</title>
+    <link rel="stylesheet" type="text/css" href="style_billetterie.css">
+</head>
+<body>
+<div class="image-bandeau">
+    <img src="img/fond_2.jpg" alt="bandeau">
+    <h1 class="titre-billetterie">Billetterie</h1>
+</div>
+
+<div class="billet">
+    <div class="face-avant">
+        <img src="img/billet-rencontre.png" alt="billet-rencontre">
+    </div>
+    <div class="face-arriere">
+      <p>Ce billet comprend une entrée prioritaire avec une rencontre des artistes à la fin du spectacle. <br>
+      Limité à 20 billets ! <br>
+      Ceci est une prévente, aucun paiement en ligne ne peut être effectué. 
+    Le paiement s’effectuera sur place le jour du spectacle.
+    Le nombre de préventes disponibles correspond aux nombres de places totales pour l’évènement. </p>
+
+    </div>
+</div>
+
+
+</body>
+</html>
