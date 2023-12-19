522daeb845ef7362554664e727f820f6c96fb8e9
Hélie <dhelie@aol.com>
2023-12-19 17:37:20 +0100

    Integration du formulaire de connection

--- header.php
+++ header.php
@@ -36,37 +36,43 @@
     </div>
     <div id="forms-conner">
         <form id="form-conn" method="post">
+            <h4>J’ai déja un compte</h4>
             <div>
-                <label for="emaila1">Email</label>
-                <input id="emaila1" type="text" name="email" placeholder="Email" required>
+                <label for="emaila1">E-mail</label>
+                <input id="emaila1" type="email" name="email" placeholder="Mon addresse e-mail" required>
             </div>
             <div>
                 <label for="passworda1">Mot de passe</label>
-                <input id="passworda1" type="password" name="password" placeholder="Mot de passe" required>
+                <input id="passworda1" type="password" name="password" placeholder="Mon mot de passe" required>
             </div>
-            <div>
+            <div class="cookies_cb_div">
                 <input id="cookiesa1" type="checkbox" name="cookieAccepted">
-                <label for="cookiesa1">J'accepte les cookies</label>
+                <label for="cookiesa1">J’acceptes l’utilisation utilisation de cookies pour ne pas à avoir à me reconnecter à chaque venu sur ce site.<br>Pour en savoir plus sur la manière dont nous utilisons les cookies et comment tu peux gérer tes préférences, consulte notre [lien vers la politique de confidentialité et de cookies].</label>
             </div>
             <input type="submit" value="Se connecter">
+            <p>Un problème, contactez nous a l’adresse : <a href="mailto:oui.oui@non.non">oui.oui@non.non</a></p>
         </form>
         <form id="form-create-acc" method="post">
+            <h4>Je shouaite crée un compte</h4>
             <div>
-                <label for="emaila2">Email</label>
-                <input id="emaila2" type="text" name="email" placeholder="Email" required>
+                <label for="emaila2">E-mail</label>
+                <input id="emaila2" type="email" name="email" placeholder="Mon addresse e-mail" required>
             </div>
             <div>
                 <label for="passworda2">Mot de passe</label>
-                <input id="passworda2" type="password" name="password" placeholder="Mot de passe" required>
+                <input id="passworda2" type="password" name="password" placeholder="Mon mot de passe" required>
             </div>
             <div>
-                <label for="usernamea2"></label>
-                <input id="usernamea2" type="text" name="username" placeholder="Username" required>
+                <label for="usernamea2">Nom d’utilisateur</label>
+                <input id="usernamea2" type="text" name="username" placeholder="Mon nom ou psuedo" required>
             </div>
-            <input type="number" name="icon_id" placeholder="Icone" required>
             <div>
+                <label for="usernamea2">Je choisis mon icone</label>
+                <input type="number" name="icon_id" placeholder="Icone" required>
+            </div>
+            <div class="cookies_cb_div">
                 <input id="cookiesa2" type="checkbox" name="cookieAccepted">
-                <label for="cookiesa2">J'accepte les cookies</label>
+                <label for="cookiesa2">J’acceptes l’utilisation utilisation de cookies pour ne pas à avoir à me reconnecter à chaque venu sur ce site.<br>Pour en savoir plus sur la manière dont nous utilisons les cookies et comment tu peux gérer tes préférences, consulte notre [lien vers la politique de confidentialité et de cookies].</label>
             </div>
             <input type="submit" value="Crée mon compte">
         </form>
