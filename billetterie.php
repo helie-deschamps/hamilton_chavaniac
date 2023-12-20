d4d9949c2d6188231492deebf9117b7b7260f6ed
Hélie <dhelie@aol.com>
2023-12-20 16:56:54 +0100

    Ajout du design systeme a toutes les pages

--- billetterie.php
+++ billetterie.php
@@ -7,6 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Billetterie</title>
+    <link rel="stylesheet" type="text/css" href="styles/design_system.css">
     <link rel="stylesheet" type="text/css" href="styles/fonts.css">
     <link rel="stylesheet" type="text/css" href="styles/style_billetterie.css">
 </head>
