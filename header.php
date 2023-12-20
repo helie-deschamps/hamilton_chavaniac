1ded3667fd0754e79d8ff47270a06beacf75fd42
Hélie <dhelie@aol.com>
2023-12-20 19:58:04 +0100

    correction d'un mauvais affichage des réservations

--- header.php
+++ header.php
@@ -38,23 +38,23 @@
                 <?php
                 if($user->reservation->countParticipants(105) > 0):?>
                     <div>
-                        <p class="ci_tp_title">Nombre de place au tarif jeunes (<b>105€</b>)</p>
+                        <p class="ci_tp_title">Nombre de place au tarif rencontre (<b>105€</b>)</p>
                         <p class="ci_tp_dash">----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------</p>
                         <p class="ci_tp_quantity"><?= $user->reservation->countParticipants(105) ?></p>
                     </div>
                 <?php endif;?>
                 <div>
-                    <p class="ci_tp_title">Nombre de place au tarif étudiants (<b>70€</b>)</p>
+                    <p class="ci_tp_title">Nombre de place au tarif normal (<b>70€</b>)</p>
                     <p class="ci_tp_dash">----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------</p>
                     <p class="ci_tp_quantity"><?= $user->reservation->countParticipants(70) ?></p>
                 </div>
                 <div>
-                    <p class="ci_tp_title">Nombre de place au tarif normal (<b>50€</b>)</p>
+                    <p class="ci_tp_title">Nombre de place au tarif étudiant (<b>50€</b>)</p>
                     <p class="ci_tp_dash">----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------</p>
                     <p class="ci_tp_quantity"><?= $user->reservation->countParticipants(80) ?></p>
                 </div>
                 <div>
-                    <p class="ci_tp_title">Nombre de place au tarif normal (<b>40€</b>)</p>
+                    <p class="ci_tp_title">Nombre de place au tarif enfant (<b>40€</b>)</p>
                     <p class="ci_tp_dash">----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------</p>
                     <p class="ci_tp_quantity"><?= $user->reservation->countParticipants(40) ?></p>
                 </div>
@@ -62,7 +62,7 @@
                         if($prix[0] != 105 && $prix[0] != 70 && $prix[0] != 50 && $prix[0] != 40):
                  ?>
                     <div>
-                        <p class="ci_tp_title">Nombre de place au tarif normal (<b><?= $prix[0]?>€</b>)</p>
+                        <p class="ci_tp_title">Nombre de place au tarif spécial de <b><?= $prix[0]?>€</b></p>
                         <p class="ci_tp_dash">----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------</p>
                         <p class="ci_tp_quantity"><?= $user->reservation->countParticipants($prix[0]) ?></p>
                     </div>
