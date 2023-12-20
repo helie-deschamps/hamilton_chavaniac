8ed3d717f2e5c5b08d32928d1ede1128863a4122
rafaelynk <rafael.yenokyan1@gmail.com>
2023-12-20 16:45:43 +0100

    ajout texte

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-include_once __DIR__ . '/private/authentication_systeme/try_auto_connection.php';
+//include_once __DIR__ . '/private/authentication_systeme/try_auto_connection.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,12 +17,18 @@ include "header.php";
 ?>
     <h1>Hamilton, la comédie musicale</h1>
 
-    <figure>
-        <img src=" " alt=""> 
+    <figure class=imageindex>
+        <img src="img/imageindex.webp" alt="photo accueil"> 
         <figcaption>Une première en France</figcaption>
     </figure>
 
-    <p>biuezgkqhjvdhizhd</p>
+    <p>Nous sommes ravis de vous accueillir pour un événement historique et culturel sans précédent : la première représentation française de la comédie musicale révolutionnaire, Hamilton. Cette production acclamée, qui a captivé le cœur d'audiences à travers le monde, arrive enfin en France, dans le cadre majestueux du Château Lafayette.
+
+Hamilton est plus qu'une comédie musicale, c'est une expérience immersive qui transporte le public dans l'effervescence de l'histoire américaine. Avec un mélange innovant de musique hip-hop, jazz, R&B et Broadway, Hamilton raconte l'histoire du père fondateur Alexander Hamilton de manière actuelle et captivante, une histoire de pouvoir, de politique, et de la passion qui a façonné une nation.
+
+Le Château Lafayette, un lieu empreint d'histoire et d'élégance, offre le décor parfait pour cette première française. Son architecture grandiose et ses jardins somptueux complètent l'éclat et l'audace de la production, offrant aux spectateurs une expérience théâtrale inoubliable.
+
+Nous vous invitons à nous rejoindre pour célébrer l'art, l'histoire et le génie musical. Les billets sont désormais disponibles pour cette série de représentations limitées. Réservez votre place et soyez témoin de l'histoire en marche, ici, au cœur de la France, au Château Lafayette.</p>
  
     <div class="carousel">
         <button class="carousel-button prev">Précédent</button>
