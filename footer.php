c7411608e929510c9b4a7a312cf063a861d569ec
rafaelynk <rafael.yenokyan1@gmail.com>
2023-12-21 11:41:39 +0100

    création du footer

---
+++ footer.php
@@ -0,0 +1,29 @@
+
+<footer class="site-footer">
+    <div class="footer-content">
+        <div class="footer-section about">
+            <h3>À propos de Hamilton</h3>
+            <p>Hamilton est une comédie musicale innovante mêlant histoire, musique et culture. Découvrez l'histoire jamais racontée d'Alexander Hamilton.</p>
+            <h3>Mentions légales</h3>
+            <p>Hébergement : alwaysdata</p>
+            <p>Contact : hamilton.chavaniac@gmail.com</p>
+            <p>Siège social SAMC : 2 Rue Antoine Martin, 43000 Le Puy-en-Velay</p>
+        </div>
+        <div class="footer-section links">
+            <h3>Liens Utiles</h3>
+            <ul>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">SAMC</a></li>
+                <li><a href="#">Billeterie</a></li>
+            </ul>
+        </div>
+        <div class="footer-section social">
+            <h3>Notre FAQ</h3>
+            <a href="faq.html">Aller à la FAQ</a>
+        </div>
+    </div>
+        
+    <div class="footer-bottom">
+        <p>© 2023 Hamilton Musical. Tous droits réservés.</p>
+    </div>
+</footer>
