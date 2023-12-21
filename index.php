50c9b229573249353e0fd9b6b623e49058bc2680
rafaelynk <rafael.yenokyan1@gmail.com>
2023-12-21 11:53:59 +0100

    ajout du footer

--- index.php
+++ index.php
@@ -70,6 +70,7 @@ Nous vous invitons à nous rejoindre pour célébrer l'art, l'histoire et le gé
     <iframe width="919" height="517" src="https://www.youtube.com/embed/DSCKfXpAGHc" title="Hamilton | Official Trailer | Disney+" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 </div>    
 
+<?php include_once"footer.php" ?>
 
 </body>
 </html>
